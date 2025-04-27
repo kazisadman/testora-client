e390cbdff9634b15a623e4a5df2c367cd671fa97
kazisadman <kazisadman01@gmail.com>
2025-04-27 22:39:00 +0600

    add domain name in axios

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://testora-server.vercel.app/api/v1",
   withCredentials: true,
 });
 export default axiosInstance;
